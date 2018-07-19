4a57949548bffaa5c65800d7f43ab3444a62f916
Mark Ross <krazkidd@gmail.com>
2018-07-19 00:12:04 -0700

    Add license notice to new file.

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,3 +1,21 @@
+// This file is part of 204∞.
+//
+// Copyright © 2018 Mark Ross <krazkidd@gmail.com>
+//
+// 204∞ is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// 204∞ is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with 204∞.  If not, see <http://www.gnu.org/licenses/>.
+
+
 Game = function (boardSize, numRandos) {
   // private ///////////////////////////////
 
